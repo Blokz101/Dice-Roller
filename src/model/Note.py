d2d764c4cd202f25d9957789834d3066dd4f3e0d
Damion Winders <130799691+Dwinders4348@users.noreply.github.com>
2025-08-14 23:28:24 -0400

    Updated Note to process its description

--- src/model/Note.py
+++ src/model/Note.py
@@ -1,10 +1,21 @@
 from src.model.Instruction import Instruction
-
+import re
 
 class Note:
 
     def __init__(self, name: str = "", raw_text: str = ""):
         self.name: str = name
-        self.raw_text: str = raw_text
+        #self.raw_text: str = raw_text
         self.text: str = ""
         self.inst_list: list[Instruction] = []
+
+        #----------------------------------
+        #This is a temporary patch that should be replaced with more thorough parsing
+        x = re.split("[\[].*[\]]", raw_text)
+        self.raw_text: str = x[0]
+
+        #-----------------------------------
+
+        
+
+
